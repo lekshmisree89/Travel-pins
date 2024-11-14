49f7c39770bfa56479fb81bcf0ce47c3ebc2a104
lekshmisree@gmail.com <lekshmisree89@gmail.com>
2024-11-14 13:43:20 -0500

    modified user model

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,17 +1,15 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IUser extends Document {
-  firstName: string;
-  lastName: string;
+  name: string;
   email: string;
   password: string;
 }
 
 const userSchema: Schema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  name: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true }
 });
 
 const User = mongoose.model<IUser>('User', userSchema);
