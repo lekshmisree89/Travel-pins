1fb7fdfa915080efaefc6acd7088cbf6866bd771
Aditi Rai <aditi45@gmail.com>
2024-11-14 23:14:50 +0530

    added deleteuser functiom in user controllers

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -57,8 +57,17 @@ const userController = {
   },
 
   // Delete a user by ID
-  
-};
+  async deleteUser(req: Request, res: Response): Promise<Response> {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      if (!user) return res.status(404).json({ error: 'User not found' });
+      return res.status(200).json({ message: 'User deleted successfully', user });
+    }
+    catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Failed to delete user' });
+  }
+},
 
 
 export default userController;
