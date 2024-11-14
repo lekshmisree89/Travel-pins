78b2ebfd87fdd43cbd49cc1d4190195b49b93af7
Aditi Rai <aditi45@gmail.com>
2024-11-14 23:42:24 +0530

    added addCountry mutation to resolvers.ts

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,8 +1,9 @@
-import { User } from '../models/index.js';
+import { User, Country } from '../models/index.js';
 
 export const resolvers = {
   Query: {
     users: async () => await User.find(),
+    country: async () => await Country.find()
   },
   Mutation: {
     addUser: async (_: any, { name, email }: { name: string; email: string }) => {
@@ -10,7 +11,10 @@ export const resolvers = {
       await newUser.save();
       return newUser;
     },
+    addCountry: async (_: any, { name, code }: { name: string; code: string }) => {
+      const newCountry = new Country({ name, code });
+      await newCountry.save();
+      return newCountry;
   },
+}
 };
-
-
