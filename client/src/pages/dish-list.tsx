ef14e89ee7568a31290f534f9c1b94105ca2894b
Ryan Merlini <rdmerlini@gmail.com>
2024-11-14 14:04:06 -0500

    dish-list pseudocoding

---
+++ client/src/pages/dish-list.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useState } from 'react'
+import { Plus, Edit2, Check, X, Utensils, Book, LogOut } from 'lucide-react'
+
+//TODO: add the dish list component
+
+
+//todo: country card component
+
+//todo: addcountryform slash button
+
+
+
+
