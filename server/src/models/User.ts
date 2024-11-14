c6a0ce2f6abfd74f912ecef3f95dd3d808ccb85a
lekshmisree@gmail.com <lekshmisree89@gmail.com>
2024-11-14 15:01:54 -0500

    modified the user models to fix errore

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,18 +1,65 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import { Schema, model} from 'mongoose';
+import bcrypt from 'bcrypt';
 
-export interface IUser extends Document {
-  name: string;
+// import schema from Book.js
+
+import mongoose from 'mongoose';
+
+
+
+//import Country from './Country';
+
+
+export interface User extends mongoose.Document {
+  id: string;
+  username: string;
   email: string;
   password: string;
+ 
+
 }
 
-const userSchema: Schema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true }
+const userSchema = new Schema<User>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    // set savedBooks to be an array of data that adheres to the bookSchema
+
+  // set this to use virtual below
+  },
+);
+
+// hash user password
+userSchema.pre<User>('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
 });
 
-const User = mongoose.model<IUser>('User', userSchema);
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password: string) {
+  return await bcrypt.compare(password, this.password);
+};
 
-export  default User;
+// when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
 
+
+const User = model<User>('User', userSchema);
+
+export default User;
