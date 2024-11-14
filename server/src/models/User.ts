260d76756829ff596bcc981f60899f30790ff767
Ryan Merlini <rdmerlini@gmail.com>
2024-11-14 11:25:22 -0500

    edited User schema to include interface of first/last/email/pwd

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,13 +1,17 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IUser extends Document {
-  name: string;
+  firstName: string;
+  lastName: string;
   email: string;
+  password: string;
 }
 
 const userSchema: Schema = new Schema({
-  name: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 const User = mongoose.model<IUser>('User', userSchema);
