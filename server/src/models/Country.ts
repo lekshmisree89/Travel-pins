f042282732d0e97ee3f6df5888b169ec40504545
Aditi Rai <aditi45@gmail.com>
2024-11-15 22:18:05 +0530

    changes to model and seeds to resolve errors

--- server/src/models/Country.ts
+++ server/src/models/Country.ts
@@ -16,15 +16,16 @@ const dishSchema: Schema = new Schema({
     required: true 
   }
 },
-{ _id: false,
-  toJSON: { getters: true },
+{ toJSON: { getters: true },
   toObject: { getters: true }, 
   timestamps: true },
 );
 
+const Dishes = model<IDishes>('Dishes', dishSchema);
+
 const countrySchema: Schema = new Schema({
     name: { type: String, required: true },
-    dishes: { type: [dishSchema], required: true },
+    dishes: { type: [], required: true },
     notes: { type: String, required: true },
   });
   
