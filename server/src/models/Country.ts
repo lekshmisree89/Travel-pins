80e34ae549705b581419250985e0fc83444d66d3
Aditi Rai <aditi45@gmail.com>
2024-11-15 23:12:49 +0530

    removed dishes subdoc from country.ts and converting to model

--- server/src/models/Country.ts
+++ server/src/models/Country.ts
@@ -1,31 +1,27 @@
 import { Schema, model, Document } from 'mongoose';
 
-export interface IDishes extends Document {
+interface ICountry extends Document {
   name: string;
-}
-
-export interface ICountry extends Document {
-  name: string;
-  dishes: IDishes[];
+  dishes: Schema.Types.ObjectId[];
   notes: string;
 }
 
-const dishSchema: Schema = new Schema({
-  name: { 
-    type: String, 
-    required: true 
-  }
-},
-{ toJSON: { getters: true },
-  toObject: { getters: true }, 
-  timestamps: true },
-);
+// const dishSchema: Schema = new Schema({
+//   name: { 
+//     type: String, 
+//     required: true 
+//   }
+// },
+// { toJSON: { getters: true },
+//   toObject: { getters: true }, 
+//   timestamps: true },
+// );
 
 // model<IDishes>('Dishes', dishSchema);
 
 const countrySchema: Schema = new Schema({
     name: { type: String, required: true },
-    dishes: { type: [dishSchema], required: true },
+    dishes: { type: Schema.Types.ObjectId, ref: 'Dishes', required: true },
     notes: { type: String, required: true },
   });
   
