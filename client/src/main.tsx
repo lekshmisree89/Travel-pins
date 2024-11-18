7c002b222c615a3dcdc697e59c8b9d0c16830ad0
lekshmisree@gmail.com <lekshmisree89@gmail.com>
2024-11-17 20:17:34 -0500

    routes have been modified in main as i have added routes for home and explore seperately

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,22 +5,34 @@ import { ExplorePage } from './pages/ExplorePage'; // Correct path
 import { SavedDishesPage } from './pages/SavedDishesPage'; // Create this page for saved dishes
 import App from './App'; // Main app container
 import {ProtectedRoute}from './components/ProtectedRoutes'; // Correct path
+import Home from './pages/Home.tsx';
 
 // Define the router with paths and components
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <App />, // Wrap the App component in ProtectedRoute
     errorElement: <h1 className="display-2">Page not found!</h1>, // Handle invalid paths
+    
+
+    
     children: [
+     
+
+    
+
       {
+        path: '/home', // The path for the home page  
         index: true, // The default route, shows the ExplorePage at '/'
-        element: <ProtectedRoute><ExplorePage /></ProtectedRoute>
+        element: <ProtectedRoute><Home/></ProtectedRoute>
       },
 
       {
-        path: '/dishlist', // The path for saved dishes
-        element: <SavedDishesPage  />, // Pass the required 'savedDishes' prop
+        path: '/explore', // The path for saved dishes
+        element:  <ProtectedRoute><ExplorePage /></ProtectedRoute>,
+
+        
+        // Pass the required 'savedDishes' prop
       },
       
       {
